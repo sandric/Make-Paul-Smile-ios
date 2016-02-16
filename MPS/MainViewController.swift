ee0d25bc1dd130a29fbdd29b938b0f2575837071
sandric <kyrychenkoanton@gmail.com>
2016-02-16 16:35:13 +0200

    Initial commit of swift implementing

---
+++ MPS/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MPS
+//
+//  Created by sandric on 16.02.16.
+//  Copyright © 2016 sandric. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
