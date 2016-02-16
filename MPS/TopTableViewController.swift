09d78dc759adb1504bf08cb3f050cb1d36be5080
sandric <kyrychenkoanton@gmail.com>
2016-02-16 20:03:53 +0200

    Implement top games

--- MPS/TopTableViewController.swift
+++ MPS/TopTableViewController.swift
@@ -8,88 +8,81 @@
 
 import UIKit
 
-class TopTableViewController: UITableViewController {
+import Alamofire
 
+
+class TopTableViewController: UITableViewController {
+    
+    var topGames:[[String : AnyObject]] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.fetchTopGames()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.topGames.count
     }
-
-    /*
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("TopCell", forIndexPath: indexPath)
+        
+        let topGame = self.topGames[indexPath.row]
+        
+        let group = topGame["group"] as? String
+        let username = topGame["username"] as? String
+        let score = topGame["score"] as? Int
+        
+        let groupLabel:UILabel = cell.viewWithTag(1) as! UILabel
+        groupLabel.text = group
+        
+        let usernameLabel:UILabel = cell.viewWithTag(2) as! UILabel
+        usernameLabel.text = username
+        
+        let scoreLabel:UILabel = cell.viewWithTag(3) as! UILabel
+        scoreLabel.text = "\(score!)"
+                
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    func fetchTopGames () {
+        Alamofire.request(.GET, "http://localhost:8080/api/top")
+            .responseJSON { response in
+                if let topResults = response.result.value as? Array<Dictionary<String,AnyObject>> {
+                    self.topGames = topResults;
+                    self.tableView.reloadData()
+                }
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    
+    @IBAction func refreshBarButtonPressed(sender: UIBarButtonItem) {
+        self.fetchTopGames()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
